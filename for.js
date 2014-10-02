d225dd4eccd9daef7576d8fe5f178f476d6e0900
codemeup <laurakemp@live.co.uk>
2014-10-02 10:24:17 -0700

    revised with bonus missed previously

--- for.js
+++ for.js
@@ -1,6 +1,17 @@
+// var Friends = ["Meg", "Ads", "Bea"];
+// var length = (Friends.length);
+// for (var i=0; i<(Friends.length); i++) {
+// 	console.log("\"Hello, " + Friends[i] + "!\"");	
+// }
+
+// version with bonus: Have the previous person say hello to the current person.
+
 var Friends = ["Meg", "Ads", "Bea"];
 var length = (Friends.length);
 for (var i=0; i<(Friends.length); i++) {
-	console.log("\"Hello, " + Friends[i] + "!\"");
+	if (i === 0){
+		console.log("\"Hello, " + Friends[i] + "!\"");
+	} else {
+	console.log("\"" + Friends[i-1] + " says Hello, " + Friends[i] + "!\"");
+	}
 }
-
