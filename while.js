87ffbe9964150ab988588fcf6e510b3cb363592a
codemeup <laurakemp@live.co.uk>
2014-10-01 22:54:08 -0700

    revised

--- while.js
+++ while.js
@@ -1,10 +1,10 @@
 var i = 99;
 while(i>0) {
 	if(i<=1) {
-		console.log(i + " bottle of beer on the wall, " + i + " bottle of beer. Take one down, pass it around, no bottles of beer on the wall...");
+		console.log(i + " bottle of beer on the wall, " + (i) + " bottle of beer. Take one down, pass it around, no bottles of beer on the wall...");
 		break;
 	} else {
-		console.log(i + " bottles of beer on the wall, " + i + " bottles of beer. Take one down, pass it around, " + i + " bottles of beer on the wall...");
+		console.log(i + " bottles of beer on the wall, " + (i) + " bottles of beer. Take one down, pass it around, " + (i-1) + " bottles of beer on the wall...");
 	i = i - 1;
 	}
 }
