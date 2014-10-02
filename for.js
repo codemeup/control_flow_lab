6eb8827c6609b1d491784cb3cfa5b94ae676a00a
codemeup <laurakemp@live.co.uk>
2014-10-01 19:03:41 -0700

    Finoto

---
+++ for.js
@@ -0,0 +1,6 @@
+var Friends = ["Meg", "Ads", "Bea"];
+var length = (Friends.length);
+for (var i=0; i<(Friends.length); i++) {
+	console.log("\"Hello, " + Friends[i] + "!\"");
+}
+
---
+++ ifElse.js
@@ -0,0 +1,9 @@
+var Foods = ["pain", "vin", "Boursin"];
+
+if(Foods.length === 0) {
+	console.log("you have no food");
+	} else if (Foods.length == 1){
+	console.log("you have one food");
+	} else {
+	console.log("You have lots of food");
+}
